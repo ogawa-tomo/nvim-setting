caf2d29e6ea803bf8f6b2e47874613f758f12538
tomo <o_tomo_1990@yahoo.co.jp>
2026-04-22 21:46:23 +0900

    コードブロック折りたたまない設定

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,3 +27,4 @@ vim.opt.winbar = "%#WinBarBold#%f%* %m"
 vim.opt.termguicolors = true
 vim.opt.winblend = 0 -- ウィンドウの不透明度
 vim.opt.pumblend = 0 -- ポップアップメニューの不透明度
+vim.opt.conceallevel = 0 -- mdファイルでコードブロックを折りたたまない
