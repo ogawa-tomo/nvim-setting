a47e3187c5913d63725a483dd694f96fbdaa4612
tomo <o_tomo_1990@yahoo.co.jp>
2026-04-26 20:32:20 +0900

    Gitのコマンド説明を追加

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -14,4 +14,8 @@ return {
     date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
     virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
   },
+  keys = {
+    { "<leader>gc", "<cmd>GitBlameCopyCommitURL<cr>", desc = "copy commit url" },
+    { "<leader>gy", "<cmd>GitBlameCopyFileURL<cr>", desc = "copy file url" },
+  },
 }
