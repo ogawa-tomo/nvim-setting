580f23f8999fb9f48d936a5c6abe4163cc1870a1
to.ogawa <to.ogawa@mtadminnoMacBook-Pro-4.local>
2026-08-07 17:05:50 +0900

    プロジェクトルートより上でNeovimを開いたとき、goplsが壊れないようにする

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,18 @@ return {
       inlay_hints = {
         enabled = false,
       },
+      servers = {
+        gopls = {
+          -- cmd_cwdは静的な文字列で、動的に解決されるroot_dirに追従できない。
+          -- そのため、この時点で解決済みのconfig.root_dirをプロセスのcwdとして
+          -- 使うカスタムcmdでgoplsを起動する。これをしないと、goplsはNeovim自身の
+          -- cwdを引き継いでしまい、プロジェクトルートより上(例: ~/dev)で
+          -- Neovimを開いた場合に`go list`が壊れる。
+          cmd = function(dispatchers, config)
+            return vim.lsp.rpc.start({ "gopls" }, dispatchers, { cwd = config.root_dir })
+          end,
+        },
+      },
     },
   },
 }
