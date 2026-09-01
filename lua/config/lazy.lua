20b832cb64518c863c645ac746cd6c0a1e1e5ad5
ogawa-tomo <to.ogawa@matsuri-tech.com>
2026-09-01 09:37:13 +0900

    TypeScriptを追加

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { "simeji/winresizer" },
     -- import/override with your plugins
     { import = "plugins" },
