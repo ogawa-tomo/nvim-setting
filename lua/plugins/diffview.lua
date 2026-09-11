9801ef42ce3c0126c36798bbe9f23e0e3c1afd5d
tomo <o_tomo_1990@yahoo.co.jp>
2026-09-12 08:14:04 +0900

    diffviewのキーマップを変更

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,9 +1,26 @@
 return {
-  "sindrets/diffview.nvim",
-  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
-  keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff View Open" },
-    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diff View Close" },
-    { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Diff View File History" },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gdd", "<cmd>DiffviewOpen<cr>", desc = "Open" },
+      { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close" },
+      { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+      {
+        "<leader>gdl",
+        ":DiffviewFileHistory<cr>",
+        mode = "v",
+        desc = "Line History",
+      },
+      { "<leader>gdr", "<cmd>DiffviewFileHistory<cr>", desc = "Repo History" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>gd", group = "diffview" },
+      },
+    },
   },
 }
