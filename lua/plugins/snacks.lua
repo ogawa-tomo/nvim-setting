8c3a3e285d153f5810a6d67ee52769cb0545bcce
tomo <o_tomo_1990@yahoo.co.jp>
2026-09-16 22:22:45 +0900

    lazygitからファイルに遷移したときタブを増やさない

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,5 +11,15 @@ return {
         },
       },
     },
+    lazygit = {
+      config = {
+        os = {
+          -- デフォルトの nvim-remote プリセットは編集のたびに新しいタブを開くため、
+          -- 同じウィンドウでファイルを開き直すコマンドで上書きする
+          edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+          editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+        },
+      },
+    },
   },
 }
